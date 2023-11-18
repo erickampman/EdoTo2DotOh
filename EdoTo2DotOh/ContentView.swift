0036d2b842a6b8f903ce2fdf482b980637c0840a
Eric Kampman <erickampman@yahoo.com>
2023-11-18 14:37:19 -0800

    Initial Commit

---
+++ EdoTo2DotOh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EdoTo2DotOh
+//
+//  Created by Eric Kampman on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EdoTo2DotOh/EdoTo2DotOhApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdoTo2DotOhApp.swift
+//  EdoTo2DotOh
+//
+//  Created by Eric Kampman on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct EdoTo2DotOhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
